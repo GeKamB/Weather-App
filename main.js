9e3dfc35bd17e1a243c5a0f29dcc860fa7044be1
GeKamB <k.gebski1@gmail.com>
2024-01-28 15:17:07 +0000

    renderCurrentWeather

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import './style.css';
 import { getWeather } from '../weather';
+import { renderCurrentWeather, renderDailyWeather } from '../render';
 
-getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather)
+getWeather(53.4809, -2.2374, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).catch(e => {
+    console.error(e)
+    alert('Error downloading weather.')
+})
 
+
+function renderWeather({ current, daily, hourly}) {
+    renderCurrentWeather(current)
+    renderDailyWeather(daily)
+ //  renderHourlyWeather(hourly)
+    document.body.classList.remove('blurred')
+}
